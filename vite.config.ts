5996e186f92cff5ebd1e9ee4bc46b37d61c548bb
Oxrial <35791730+Oxrial@users.noreply.github.com>
2025-05-20 10:44:54 +0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ const DRIVE_LETTER_REGEX = /^[a-z]:/i
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd())
+	console.log(env)
 	return {
 		base: env.VITE_PUBLIC_PATH,
 		server: {
